a57b9df629f576aafb3c6993a58fe066c3f99e4e
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-10-15 18:41:33 +0700

    Add files via upload

---
+++ TUGAS_MINGGU_KE-4/E41191560_Anggito Suryo Baskoro_gol C/tugas_1/tes.php
@@ -0,0 +1,22 @@
+<?php
+$tes = "Saya Pergi Kerja";
+$bilangan_pertama = 12;
+$bilangan_kedua = 78;
+$angka = 12.177;
+$x = false;
+$y = true;
+$anggota = array("Andi","budi","joni");
+echo $tes; 
+echo "<br>";
+echo $bilangan_pertama;
+echo "<br>";
+echo $bilangan_kedua;
+echo "<br>";
+echo $angka;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+echo $anggota[1];
+?>
