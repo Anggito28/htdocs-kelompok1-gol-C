927f45e0455b7a3037cb80cd82adc4400bdb4702
Achmad Aji Wicaksono <ajiw491@gmail.com>
2020-10-30 21:33:29 +0700

    upload

---
+++ TUGAS_MINGGU_KE_6/Achmad Aji Wicaksono_E41191405_Gol C_Minggu6/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+//tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+//tipe data string (teks)
+$nama_lengkap = "Achmad Aji Wicaksono";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 49;
+
+//tipe data float
+$tinggi = 174;
+
+//tipe data boolean
+$menikah = false;
+
+echo "Nama : $nama_lengkap<br>";
+echo "Jenis Kelamin : $jenis_kelamin<br>";
+echo "Umur : $umur<br>";
+echo "Berat Badan : $berat<br>";
+echo "Tinggi Badan : $tinggi<br>";
+echo "Status Menikah : $menikah";
+?>
