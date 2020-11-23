6f6dff992a330a7ec254457de3d208445ff880cc
Danang ardianto <ardhyanto6629@gmail.com>
2020-11-23 09:55:10 +0700

    upload tugas php

---
+++ TUGAS_MINGGU_KE-6/Danang_Ardianto_E41191453_PHP/belajar_php/echo.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo ("Hello world!<br>");
+echo "Aku sedang belajar PHP!<br>";
+echo "Ini", "teks", "yang", "dibuat", "terpisah";
+
+?>
+
+<?php  ?>
---
+++ TUGAS_MINGGU_KE-6/Danang_Ardianto_E41191453_PHP/belajar_php/tipe_data.php
@@ -0,0 +1,29 @@
+<?php
+
+// tipe data char
+$jenis_kelamin = 'L';
+
+// tipe data string
+$nama_lengkap = 'petani kode';
+
+// tipe data integer
+$umur = 20;
+
+// tipe data float
+$berat = 48.3;
+
+// tipe data float
+$tinggi = 182.2;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama: $nama_lengkap <br>";
+echo "Jenis Kelamin: $jenis_kelamin <br>";
+echo "Umur: $umur tahun <br>";
+echo "Berat badan: $berat kg <br>";
+echo "Tinggi badan: $tinggi cm <br>";
+echo "Menikah: $menikah";
+
+?>
+<?php  ?>
