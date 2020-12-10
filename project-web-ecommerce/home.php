f44663c848ed0cdff49be50f4dcf45ebb522c101
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-10 13:52:54 +0700

    tambahan

--- project-web-ecommerce/home.php
+++ project-web-ecommerce/home.php
@@ -47,6 +47,7 @@
                             </a>
                             <div class="dropdown-menu mt-3" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="#">Tentang Kami</a>
+                                <a class="dropdown-item" href="#">Blog</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">Pengiriman</a>
                                 <a class="dropdown-item" href="#">Pembayaran</a>
@@ -66,7 +67,48 @@
             </nav>
         </div>
     </section>
-
+    <section class="content">
+        <div class="container">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="asset/img/bonsai1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="asset/img/bonsai3.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="asset/img/bonsai4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+        </div>
+    </section>
     <section class="content">
         <div class="container">
 
@@ -83,9 +125,9 @@
                                         Kategori
                                     </button>
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
+                                        <a class="dropdown-item" href="#">Peralatan Tanaman</a>
+                                        <a class="dropdown-item" href="#">Bonsai</a>
+                                        <a class="dropdown-item" href="#">Pupuk dan Racun</a>
                                     </div>
                                 </div>
 
