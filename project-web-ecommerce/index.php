bd7a5ab372aa5d81acfceff2e3afc9a11bf9677b
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-10 16:31:14 +0700

    tambahan

--- project-web-ecommerce/index.php
+++ project-web-ecommerce/index.php
@@ -7,25 +7,216 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
 
-    <title>Hello, world!</title>
+    <!-- custom style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Produk - Rudi Bonsai</title>
 </head>
 
-<body>
-    <h1>Hello, world!</h1>
+<body class="bg-light">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <?php include "includes/topbar.php"; ?>
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <section class="content">
+        <div class="container">
+            <!--carousel item  -->
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="asset/img/bonsai1.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="asset/img/bonsai2.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="asset/img/bonsai3.webp" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="container">
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
+            <div class="row mb-4">
+                <div class="col-lg-12">
+                    <div class="card shadow">
+                        <div class="card-body d-flex justify-content-between">
+                            <h5 class="mb-0 my-auto">Produk</h5>
+                            <div class="d-inline-flex">
+                                <div class="dropdown my-auto mr-3">
+                                    <button class="btn btn-sm btn-secondary dropdown-toggle my-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Kategori
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">Peralatan Tanaman</a>
+                                        <a class="dropdown-item" href="#">Bonsai</a>
+                                        <a class="dropdown-item" href="#">Pupuk dan Racun</a>
+                                    </div>
+                                </div>
+
+                                <form class="search form-inline my-2 my-lg-0">
+                                    <div class="input-group">
+                                        <input class="form-control" type="search" placeholder="Cari produk..." aria-label="Search">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-success my-2 my-sm-0" type="submit">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                <!-- search dropdown (small screen) -->
+                                <div class="dropdown my-auto search-sm">
+                                    <button class="btn btn-success my-2 my-sm-0" type="submit" data-toggle="dropdown">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-search px-2">
+                                        <form class="form-inline">
+                                            <input class="form-control" type="search" placeholder="Cari produk" aria-label="Search">
+                                            <button class="btn btn-success my-2 my-sm-0 d-none" type="submit">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-5">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body shadow">
+                            <div class="row konten-produk">
+
+                                <!-- list produk -->
+                                <div class="konten-item col-lg-3 col-md-4 col-sm-6 mb-4">
+                                    <div class="card shadow-sm">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <img src="asset/img/5fccd0d6cb7db.jpeg" class="product-image embed-responsive-item" alt="...">
+                                        </div>
+                                        <div class="card-body">
+                                            <h6 class="card-title">
+                                                Card title Lorem ipsum dolor sit amet.
+                                            </h6>
+                                            <hr>
+                                            <div class="input-group mb-4">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text font-weight-bold">Rp</span>
+                                                </div>
+                                                <p class="form-control font-weight-bold">
+                                                    100,000
+                                                </p>
+                                            </div>
+
+                                            <div class="text-right d-block">
+                                                <button class="btn btn-sm btn-success">
+                                                    Detail Produk
+                                                </button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <!-- pagination -->
+                                    <nav aria-label="Page navigation example">
+                                        <ul class="pagination justify-content-end">
+                                            <li class="page-item disabled">
+                                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                            </li>
+                                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                            <li class="page-item">
+                                                <a class="page-link" href="#">Next</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- footer -->
+        <?php include "includes/footer.php"; ?>
+    </section>
+
+
+    <script src="vendor/jquery/jquery-3.5.1.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            let item = $("div.konten-item");
+            let konten = $("div.row.konten-produk");
+
+            for (let i = 0; i < 7; i++) {
+                item.clone().appendTo(konten);
+
+            }
+
+            console.log(item);
+
+            $("div.search-sm").hide();
+
+            if ($(window).width() < 768) {
+                $("form.search").hide();
+                $("div.search-sm").show();
+            }
+
+            $(window).resize(function() {
+                if ($(window).width() < 768) {
+                    $("form.search").hide();
+                    $("div.search-sm").show();
+                } else {
+                    $("form.search").show();
+                    $("div.search-sm").hide();
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
