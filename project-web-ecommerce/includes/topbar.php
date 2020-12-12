0f61085c9d64573d150b803238cf896e2377c508
Danang ardianto <ardhyanto6629@gmail.com>
2020-12-12 17:17:38 +0700

    update topbar

--- project-web-ecommerce/includes/topbar.php
+++ project-web-ecommerce/includes/topbar.php
@@ -1,6 +1,6 @@
 <section class="header shadow-sm position-fixed">
     <div class="container-lg">
-        <nav class="navbar py-3 navbar-expand-lg navbar-light bg-white justify-content-between">
+        <nav class="navbar py-2 navbar-expand-lg navbar-light bg-white justify-content-between">
             <a href="index.php" class="navbar-brand d-inline-flex">
                 <img src="asset/logo/bonsai_1.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
                 <h5 class="ml-2 mt-2 font-weight-bold">Rudi Bonsai</h5>
@@ -10,7 +10,7 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav my-auto mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
@@ -21,7 +21,7 @@
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Info
                         </a>
-                        <div class="dropdown-menu mt-3" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu mb-4" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="#">Tentang Kami</a>
                             <a class="dropdown-item" href="#">Kontak</a>
                             <div class="dropdown-divider"></div>
@@ -29,9 +29,9 @@
                             <a class="dropdown-item" href="#">Pembayaran</a>
                         </div>
                     </li>
-
                 </ul>
-                <div class="d-inline-flex">
+
+                <div id="topbarCTA">
                     <a class="mr-4" href="#">
                         <i class="fa fa-shopping-cart fa-2x text-secondary"></i>
                         <!-- <span class="text">Keranjang</span> -->
@@ -43,18 +43,18 @@
                             <a class="text-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fa fa-user-circle fa-2x"></i>
                             </a>
-                            <div style="width: 240px;" class="dropdown-menu dropdown-menu-right mt-3">
+                            <div id="userMenu" style="width: 240px;" class="dropdown-menu dropdown-menu-right mt-3">
                                 <p class="d-block pl-4 pt-2">
                                     <?= $_SESSION['email']; ?>
                                 </p>
                                 <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="profil.php?id=<?= $_SESSION['id']; ?>">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
+                                    Profil
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
+                                    Pembelian
                                 </a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="logout.php">
