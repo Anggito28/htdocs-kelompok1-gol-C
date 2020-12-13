900aa6070d516f664a76a173406c02e36ff75e71
Danang ardianto <ardhyanto6629@gmail.com>
2020-12-13 22:16:37 +0700

    profil user, edit profil user, upload foto profil

--- project-web-ecommerce/config/function.php
+++ project-web-ecommerce/config/function.php
@@ -41,3 +41,98 @@ function register($data)
 
     return mysqli_affected_rows($conn);
 }
+
+function editProfil($data)
+{
+    global $conn;
+
+    $kdAkun = $data['kdAkun'];
+    $nama = $data['nama'];
+    $jenkel = $data['jenkel'];
+    $email = stripslashes($data['email']);
+    $telp = $data['telp'];
+    $prov = $data['prov'];
+    $kab = $data['kab'];
+    $kec = $data['kec'];
+    $detail = $data['detail'];
+    $imageOld = $data['image-old'];
+
+    // apakah user upload foto baru
+    if ($_FILES['image']['error'] === 4) {
+        $img = $imageOld;
+    } else {
+        $img = uploadGambar();
+        unlink("img/profile-picture/$imageOld");
+        if (!$img) {
+            return false;
+        }
+    }
+
+
+    // periksa email ganti
+    $result = mysqli_query($conn, "SELECT email FROM tb_akun WHERE kd_akun = '$kdAkun'");
+    $row = mysqli_fetch_assoc($result);
+
+    $akunUpdate = "UPDATE tb_akun SET email = '$email', foto_profil = '$img' WHERE kd_akun = $kdAkun";
+    mysqli_query($conn, $akunUpdate);
+
+    $response = mysqli_affected_rows($conn);
+
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['profil-pic'] = $img;
+
+    $update = "UPDATE tb_pembeli SET nama = '$nama', jenis_kelamin = '$jenkel', id_provinsi = '$prov', id_kabupaten = '$kab', id_kecamatan = '$kec', detail_alamat = '$detail', no_telepon = '$telp' WHERE kd_akun = $kdAkun";
+    mysqli_query($conn, $update);
+
+    $response = $response + mysqli_affected_rows($conn);
+
+    return $response;
+}
+
+function uploadGambar()
+{
+    $fileName = $_FILES['image']['name'];
+    $fileSize = $_FILES['image']['size'];
+    $error = $_FILES['image']['error'];
+    $tmpName = $_FILES['image']['tmp_name'];
+
+    // cek apakah ada gambar diupload
+    if ($error === 4) {
+        echo "
+        <script>
+            alert('No image file');
+        </script>";
+        return false;
+    }
+
+    // cek apakah yg diupload file gambar
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png', 'webp'];
+    $ekstensiGambar = explode('.', $fileName);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+
+    if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo "
+        <script>
+            alert('upload image file!!!');
+        </script>";
+        return false;
+    }
+
+    //cek ukuran file
+    if ($fileSize > 100000000 || $fileSize == 0) {
+        echo "
+        <script>
+            alert('File too big!!!');
+        </script>";
+        return false;
+    }
+
+    //gambar siap diupload
+    $newFileName = uniqid();
+    $newFileName .= '.';
+    $newFileName .= $ekstensiGambar;
+
+    move_uploaded_file($tmpName, 'img/profile-picture/' . $newFileName);
+
+    return $newFileName;
+}
