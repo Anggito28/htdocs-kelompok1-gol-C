33beff6e8dfdd744ad6de92ac32bd118bddca2fb
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-14 05:49:14 +0700

    d

---
+++ project-web-ecommerce/detailproduk.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+
+    <!-- custom style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Halaman - Rudi Bonsai</title>
+</head>
+
+<body class="bg-light">
+
+    <!-- topbar header -->
+    <?php include "includes/topbar.php"; ?>
+
+    <section class="content">
+        <div class="container">
+
+            <!-- Header konten -->
+            <div class="row mb-4">
+                <div class="col-lg-12">
+                    <div class="card shadow">
+                        <div class="card-body d-flex justify-content-between">
+
+                            <h5 class="mb-0 my-auto">Judul Halaman</h5>
+                            <div class="d-inline-flex">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-5">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body shadow">
+                            <!-- Isi card konten -->
+                            <div class="container">
+                                <div class="card mb-3" style="max-width: 720px;">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-4">
+                                            <img src="asset/img/bonsai1.jpg" class="card-img" alt="...">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- footer -->
+        <?php include "includes/footer.php"; ?>
+    </section>
+
+    <script src="vendor/jquery/jquery-3.5.1.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
