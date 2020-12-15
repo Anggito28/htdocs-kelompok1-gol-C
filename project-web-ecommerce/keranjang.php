c96863619ed3fa98119cde1180c6f38ee480b006
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-15 12:50:53 +0700

    4

---
+++ project-web-ecommerce/keranjang.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+
+    <!-- custom style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Halaman - Rudi Bonsai</title>
+</head>
+
+<body class="bg-light">
+
+    <!-- topbar header -->
+    <?php include "includes/topbar.php"; ?>
+
+    <section class="content">
+        <div class="container">
+
+            <!-- Header konten -->
+            <div class="row mb-4">
+                <div class="col-lg-12">
+                    <div class="card shadow">
+                        <div class="card-body d-flex justify-content-between">
+
+                            <h5 class="mb-0 my-auto">Judul Halaman</h5>
+                            <div class="d-inline-flex">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-5">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body shadow">
+                            <!-- Isi card konten -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- footer -->
+        <?php include "includes/footer.php"; ?>
+    </section>
+
+    <?php include "includes/scripts.php"; ?>
+
+    <!-- indikator menu aktif -->
+    <script>
+        $(document).ready(function() {
+            let topbar = "<?= $topbarActive; ?>";
+            $("#" + topbar).addClass("active");
+
+            <?php if (isset($itemActive)) : ?>
+                let collapseItem = "<?= $itemActive; ?>";
+                $("#" + collapseItem).addClass("active text-success");
+                $("#" + sidebar + " a:first").removeClass("collapsed");
+                $("#" + sidebar + " div.collapse").addClass("show");
+            <?php endif; ?>
+        });
+    </script>
+
+</body>
+
+</html>
