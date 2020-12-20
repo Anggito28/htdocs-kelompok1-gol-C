328af1526ca9d5631133dab23c1145da93e933ba
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-20 16:27:08 +0700

    Create laporan.php

---
+++ project-web-ecommerce/admin-dashboard/laporan.php
@@ -0,0 +1,100 @@
+<?php
+// dua variabel dibawah ini untuk indikator sidebar aktif
+// $sidebarActive = "";
+// $itemActive = "";
+
+// selalu ikutkan 2 file ini untuk dapat menjalankan fungsi dan konek database
+require "config/connect.php";
+require "config/function.php";
+
+?>
+
+<!-- 
+  ini adalah file template untuk membuat halaman baru di admin dashboard
+  copy isi file ini lalu paste kan ke file halaman baru yang ingin dibuat
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Blank</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom-style.css">
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <?php include "includes/sidebar.php" ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <?php include "includes/topbar.php" ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- konten halaman isi dibawah ini -->
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Laporan</h1>
+                    <div class="dropdown">
+                        <a class="btn btn-success dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">Tahunan</a>
+                            <a class="dropdown-item" href="#">Bulanan</a>
+                            <a class="dropdown-item" href="#">Harian</a>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <?php include "includes/footer.php" ?>
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?php include "includes/logout-modal.php" ?>
+
+    <?php include "includes/scripts.php" ?>
+
+</body>
+
+</html>
