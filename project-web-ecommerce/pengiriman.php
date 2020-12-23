084051ae014cbf5bc018cd34f581ef4524ad24f1
Anggito28 <71911183+Anggito28@users.noreply.github.com>
2020-12-23 19:23:33 +0700

    pengiriman

---
+++ project-web-ecommerce/pengiriman.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+
+    <!-- custom style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Halaman - Rudi Bonsai</title>
+</head>
+
+<body class="bg-light">
+
+    <!-- topbar header -->
+    <?php include "includes/topbar.php"; ?>
+
+    <section class="content">
+        <div class="container">
+
+            <!-- Header konten -->
+            <div class="row mb-4">
+                <div class="col-lg-12">
+                    <div class="card shadow">
+                        <div class="card-body d-flex justify-content-between">
+
+                            <h5 class="mb-0 my-auto">Pengiriman </h5>
+                            <div class="d-inline-flex">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-5">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body shadow">
+                            <!-- Isi card konten -->
+                            <h4>
+                                <center>Video Profil Pengiriman Indah Logistik Cargo</center>
+                            </h4>
+                            <iframe width="100%" height="480" src="https://www.youtube.com/embed/5ftD_BjHju0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <hr>
+                            <h4>
+                                <center>Profil Indah Logistik Cargo</center>
+                            </h4>
+                            <hr>
+                            <p class="text-justify"> PT. Indah Logistik merupakan suatu perusahaan yang bekerja pada bidang jasa titip barang atau pengiriman barang dalam bentuk apapun dan memiliki jangkauan pelayanan diseluruh Indonesia maupun sampai keluar negeri. PT. Indah Logistik sendiri memiliki armada ekspedisi yang dapat mengantar barang melalui Darat, Udara, dan Laut. Berikut pelayanan yang ditawarkan oleh PT. indah Logistik :
+                            </p>
+                            <p class="text-justify"><strong>1. Darat</strong></p>
+                            <p class="text-justify">Ini adalah jenis layanan yang ditawarkan secara khusus untuk memenuhi kebutuhan pelanggan terhadap pengiriman barang berskala kecil dan besar. Layanan tersebut hanya ditujukan untuk pengiriman ke daerah – daerah yang mudah dijangkau di seluruh Indonesia.</p>
+                            <p class="text-justify"><strong>2. Laut</strong></p>
+                            <p class="text-justify">Ini adalah layanan pengiriman dengan tarif cargo laut dan dengan harga sangat terjangkau sekali. Lalu untuk estimasi pengirimannya dilakukan secara tepat dan akurat dalam jumlah banyak.</p>
+                            <p class="text-justify"><strong>3. Udara</strong></p>
+                            <p class="text-justify">Ini adalah layanan pengiriman khusus buat mereka yang sudah menjadi agen cargo resmi dari sebuah maskapai terbesar di Indonesia. Melalui pelayanan via udara tersebut proses pengiriman akan dilakukan dengan cepat untuk ketepatan waktu penyampaian.</p>
+                            <p class="text-justify"><strong>4. City Courier</strong></p>
+                            <p class="text-justify">Ini adalah layanan pickup dan delivery disatu wilayah tertentu. Layanan tersebut lebih mengedepankan lead time di rentang waktu singkat, alias dalam hitungan jam.</p>
+                            <p class="text-justify"><strong>5. Kereta Api</strong></p>
+                            <p class="text-justify">Ini adalah layanan pengiriman dengan cakupan bisnis Door to Door atau DTD, untuk memaksimalkan layanan paripurna bagi pelanggan kereta api.</p>
+                            <p class="text-justify"><strong>6. Trucking</strong></p>
+                            <p class="text-justify">Ini adalah layanan pengiriman menggunkan armada mobil dan truk antar kota sampai antar pulau, dimana barang tersebut akan dikirimkan dalam jumlah besar atau carter.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- footer -->
+        <?php include "includes/footer.php"; ?>
+    </section>
+
+    <?php include "includes/scripts.php"; ?>
+
+</body>
+
+</html>
